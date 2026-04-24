d43d5a9dcb6d62cc096d0134c4374706b1963e35
Alan Newton <guitarfreak2689@gmail.com>
2026-04-24 10:14:56 +0100

    fix: expose Netlify waitlist form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 import LandingRuntime from "./LandingRuntime";
 
 export default function Home() {
-  return <LandingRuntime />;
+  return (
+    <>
+      <form
+        name="ios-waitlist"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        hidden
+      >
+        <input type="hidden" name="form-name" value="ios-waitlist" />
+        <p className="hidden"><input name="bot-field" /></p>
+        <input type="email" name="email" required />
+      </form>
+      <LandingRuntime />
+    </>
+  );
 }
